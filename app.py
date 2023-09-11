3df754424273a983f19c1191594b8d1956557e6e
José Delpino <delpinoivivas@gmail.com>
2023-09-11 00:04:16 -0500

    Improved "Design Context" docstring

--- app.py
+++ app.py
@@ -13,22 +13,33 @@ Written in Python 3.11.5 and Flask 2.3.3
 
 Design Context
 
-While addressing this challenge, I operated under the assumption that both
-this API and its encompassing microservice will eventually integrate into
-a broader application or system comprised of multiple microservices.
-Each microservice, I assume, autonomously manages its in-memory and/or
-database storage, along with other operations. Moreover, it's anticipated
-that in the future, multiple instances of this microservice will operate
+While tackling this challenge, I made the following assumptions:
+
+[1] This API along with its encompassing microservice will be better integrated
+into a broader application or system comprised of multiple microservices.
+
+[2] Each microservice autonomously manages its in-memory and/or database
+storage and other operations. Furthermore, each microservice selects
+the most fitting data storage solution tailored to its specific needs.
+
+[3] In the future, multiple instances of this microservice will operate
 concurrently for scalability.
 
 At this stage, my primary focus has been on designing the API and
-the Receipt class, as well as unittests, data validation, error handling,
-and basic error logging.
+the Receipt class, as well as unit tests, data validation, error handling,
+and foundational error logging.
 
 The Receipt class is currently responsible for processing and storing
 receipts in memory. Moving forward, it's envisaged that this class will
 extend its capabilities to handle persistent database storage of receipts
 and other advanced scalability-driven operations.
+
+Using a class to represent a receipt will also allow for easier integration
+with other classes and functions in the future. For example,
+a point calculator class could be created to handle more dynamic
+and complex rules. This dynamic rules could be fetched from a database
+or data source shared with other microservices.
+
 
 ------------------------------------------------------------------------
 """
