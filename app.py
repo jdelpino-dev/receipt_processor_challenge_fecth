d6745d626b953c0f041592fbf62ff6dd992ae950
José Delpino <delpinoivivas@gmail.com>
2023-09-10 23:47:41 -0500

    Added desing context docstring to the mnain file

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 """
+------------------------------------------------------------------------
 Receipt Processor API — Main Application
-
 Fetch Rewards Backend Engineering Apprenticeship Coding Exercise
-Solution by: José Delpino
+Solution by: José Delpino (delpinoivivas@gmail.com)
 September 2023
 
+------------------------------------------------------------------------
+
 Written in Python 3.11.5 and Flask 2.3.3
+
+------------------------------------------------------------------------
+
+Design Context
+
+While addressing this challenge, I operated under the assumption that both
+this API and its encompassing microservice will eventually integrate into
+a broader application or system comprised of multiple microservices.
+Each microservice, I assume, autonomously manages its in-memory and/or
+database storage, along with other operations. Moreover, it's anticipated
+that in the future, multiple instances of this microservice will operate
+concurrently for scalability.
+
+At this stage, my primary focus has been on designing the API and
+the Receipt class, as well as unittests, data validation, error handling,
+and basic error logging.
+
+The Receipt class is currently responsible for processing and storing
+receipts in memory. Moving forward, it's envisaged that this class will
+extend its capabilities to handle persistent database storage of receipts
+and other advanced scalability-driven operations.
+
+------------------------------------------------------------------------
 """
 
 from flask import Flask, request, jsonify
